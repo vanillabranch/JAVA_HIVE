809f1363632ea214ce13c892826299f599c306e0
vanillabranch <lockdown@kakao.com>
2021-11-02 23:48:19 +0900

    System.out.println()으로 콘솔에 출력해보기

---
+++ src/com/company/java_hive/Println.java
@@ -0,0 +1,9 @@
+package com.company.java_hive;
+
+public class Println {
+
+    public static void main(String[] args){
+        //System.out.println()으로 콘솔에 출력해보기
+        System.out.println("Hello world~!");
+    }
+}
